b6984e5a43d3fae5b23b5e0e731201a939cc097d
Reya Vir <reyavir@gmail.com>
2024-01-07 12:53:27 -0800

    changed style

--- ai_earth_hack/main.py
+++ ai_earth_hack/main.py
@@ -31,8 +31,6 @@ def main():
   designer_value = st.slider('UX/UI Designer', 0, 5, 2)
   pm_value = st.slider('Product Manager', 0, 5, 2)
   roles = [domain_expert_value, engineer_value, designer_value, pm_value]
-  if sum(roles)!= 1.0:
-    st.write("Role weights must sum to 1.")
 
   uploaded_rubric = st.file_uploader("You can upload a new rubric", 
                                   type=["txt"], 
@@ -45,20 +43,15 @@ def main():
     with open(os.path.join(os.getcwd(), "rubric.txt"),"wb") as f: 
         f.write(bytes_data)
 
-
-  uploaded_files = st.file_uploader("You can upload multiple PDF files.", 
-                                  type=["pdf"], 
-                                  accept_multiple_files=True,
-                                  label_visibility='visible')
   
   uploaded_ideas = st.file_uploader("You can upload a CSV file with ideas to be evaluated.", 
                                   type=["csv"], 
                                   accept_multiple_files=False,
                                   label_visibility='visible')
   
-  if len(uploaded_files)>0:
-    bytes_data = uploaded_files.read()
-    st.write(bytes_data)
+  # if len(uploaded_ideas)>0:
+  #   bytes_data = uploaded_ideas.read()
+  #   st.write(bytes_data)
     
   if uploaded_ideas is not None:
     # Read the file with pandas
@@ -67,18 +60,18 @@ def main():
     # Show the content of the CSV file
     st.write(df)
     
-  # Evaluate the CSV file
-  
-  ## Sample rubric
-  sample_rubric = """
-    Originality, Idea has common elements with no unique differentiation. (1), Idea shows some novel thinking and differentiation. (2), Idea is largely original, showing significant new thinking. (3), Idea is completely unique demonstrating groundbreaking thinking. (4)
-    Feasibility, Idea has significant practical or technical obstacles. (1), Idea is somewhat practical but faces notable challenges. (2), Idea is fairly practical with manageable challenges. (3), Idea is highly practical and can be implemented smoothly. (4)
-    Impact, Idea has a limited or unclear impact. (1), Idea has a moderate impact with some tangible benefits. (2), Idea has a significant impact with clear benefits. (3), Idea has a transformative impact with far-reaching benefits. (4)
-    Development & Research, Idea is underdeveloped with minimal research or supporting data. (1), Idea is somewhat developed with some research or supporting data. (2), Idea is well-developed with substantial research or supporting data. (3), Idea is fully developed with extensive research or supporting data. (4)
-    Scalability, Idea shows little to no potential for growth or adaptation. (1), Idea shows some potential for growth or adaptation. (2), Idea shows considerable potential for growth or adaptation. (3), Idea shows extensive potential for growth or adaptation. (4)
-    """
-  
-  new_df = process_dataframe_with_evaluation(df, sample_rubric)
-  st.write(new_df)
+    # Evaluate the CSV file
+    
+    ## Sample rubric
+    sample_rubric = """
+      Originality, Idea has common elements with no unique differentiation. (1), Idea shows some novel thinking and differentiation. (2), Idea is largely original, showing significant new thinking. (3), Idea is completely unique demonstrating groundbreaking thinking. (4)
+      Feasibility, Idea has significant practical or technical obstacles. (1), Idea is somewhat practical but faces notable challenges. (2), Idea is fairly practical with manageable challenges. (3), Idea is highly practical and can be implemented smoothly. (4)
+      Impact, Idea has a limited or unclear impact. (1), Idea has a moderate impact with some tangible benefits. (2), Idea has a significant impact with clear benefits. (3), Idea has a transformative impact with far-reaching benefits. (4)
+      Development & Research, Idea is underdeveloped with minimal research or supporting data. (1), Idea is somewhat developed with some research or supporting data. (2), Idea is well-developed with substantial research or supporting data. (3), Idea is fully developed with extensive research or supporting data. (4)
+      Scalability, Idea shows little to no potential for growth or adaptation. (1), Idea shows some potential for growth or adaptation. (2), Idea shows considerable potential for growth or adaptation. (3), Idea shows extensive potential for growth or adaptation. (4)
+      """
+
+    new_df = process_dataframe_with_evaluation(df, sample_rubric)
+    st.write(new_df)
   
                           
